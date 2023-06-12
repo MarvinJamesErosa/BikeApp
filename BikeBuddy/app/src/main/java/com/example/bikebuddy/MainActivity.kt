c17d613f01356b7472cbeb6a2fdc31c52ed7b460
Ram Ymac E. Llena <104246670+RamYmacLlena@users.noreply.github.com>
2023-06-12 19:19:30 +0800

    Added navigation for map page implementation

--- BikeBuddy/app/src/main/java/com/example/bikebuddy/MainActivity.kt
+++ BikeBuddy/app/src/main/java/com/example/bikebuddy/MainActivity.kt
@@ -2,17 +2,47 @@ package com.example.bikebuddy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.Fragment
+import com.example.bikebuddy.databinding.ActivityMainBinding
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         runBlocking {
             installSplashScreen()
             delay(1500)
         }
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Go())
+
+        binding.BottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.Gonav -> replaceFragment(Go())
+                R.id.Community -> replaceFragment(Community())
+                R.id.Account -> replaceFragment(Account())
+
+                else ->{
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.mapFragment,fragment)
+        fragmentTransaction.commit()
     }
 }
