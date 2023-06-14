d096f8558b7dc2eec053c8210a413a51c42000ae
Ram Ymac E. Llena <104246670+RamYmacLlena@users.noreply.github.com>
2023-06-14 17:49:46 +0800

    Update splash screen and navigation

--- BikeBuddy/app/src/main/java/com/example/bikebuddy/MainActivity.kt
+++ BikeBuddy/app/src/main/java/com/example/bikebuddy/MainActivity.kt
@@ -8,10 +8,21 @@ import androidx.fragment.app.Fragment
 import com.example.bikebuddy.databinding.ActivityMainBinding
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import android.view.View
+import android.widget.PopupWindow
+import android.view.LayoutInflater
+import android.view.ViewGroup.LayoutParams
+import android.widget.Button
+import com.example.bikebuddy.Account
+import com.example.bikebuddy.Community
+import com.example.bikebuddy.Go
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private var sharedRoutesPopup: PopupWindow? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         runBlocking {
@@ -22,27 +33,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         replaceFragment(Go())
 
-        binding.BottomNavigationView.setOnItemSelectedListener {
-
-            when(it.itemId){
-
+        binding.BottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
                 R.id.Gonav -> replaceFragment(Go())
                 R.id.Community -> replaceFragment(Community())
                 R.id.Account -> replaceFragment(Account())
-
-                else ->{
-
+                else -> {
                 }
             }
             true
         }
     }
 
-    private fun replaceFragment(fragment : Fragment){
-
+    private fun replaceFragment(fragment: Fragment) {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.mapFragment,fragment)
+        fragmentTransaction.replace(R.id.mapFragment, fragment)
         fragmentTransaction.commit()
+    }
+
+    fun goToMainScreen(view: View) {
+        // Handle the onClick event for Explore button
+        // Implement the logic to go back to the main screen here
+    }
+
+    fun showSharedRoutesDialog(view: View) {
+        // Handle the onClick event for SharedRoutes button
+        val inflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val sharedRoutesView = inflater.inflate(R.layout.sharedroutespopup, null)
+
+        sharedRoutesPopup = PopupWindow(sharedRoutesView, LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, true)
+        sharedRoutesPopup?.showAtLocation(sharedRoutesView, 0, 0, 0)
+
+        val closeButton = sharedRoutesView.findViewById<Button>(R.id.closeButton)
+        closeButton.setOnClickListener {
+            sharedRoutesPopup?.dismiss()
+        }
     }
 }
