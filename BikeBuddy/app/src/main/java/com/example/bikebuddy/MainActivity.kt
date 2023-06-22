b752ccee07d6d9663013af5a82a0141f31143da4
Yuan <yuangab10@gmail.com>
2023-06-23 03:57:53 +0800

    Fixed the bug taht crashes the app when location is turned off

--- BikeBuddy/app/src/main/java/com/example/bikebuddy/MainActivity.kt
+++ BikeBuddy/app/src/main/java/com/example/bikebuddy/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.bikebuddy
 
 import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.IntentSender
 import android.content.pm.PackageManager
-import android.graphics.Camera
-import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.provider.Settings
+import androidx.core.app.ActivityCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
 import com.example.bikebuddy.databinding.ActivityMainBinding
@@ -17,21 +20,21 @@ import android.widget.PopupWindow
 import android.view.LayoutInflater
 import android.view.ViewGroup.LayoutParams
 import android.widget.Button
-import androidx.core.app.ActivityCompat
+import androidx.appcompat.app.AlertDialog
 import com.example.bikebuddy.Account
 import com.example.bikebuddy.Community
 import com.example.bikebuddy.Go
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.CameraUpdate
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapFragment
-import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.location.*
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.CameraPosition
+
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -39,13 +42,26 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMainBinding
     private var sharedRoutesPopup: PopupWindow? = null
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var mMap: GoogleMap
-    private lateinit var lastLocation: Location
-    private lateinit var fusedLocationClient : FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
 
     companion object {
-        private const val LOCATION_REQUEST_CODE = 1
+        private const val LOCATION_REQUEST_CODE = 145
+        private const val LOCATION_SETTINGS_REQUEST_CODE = 25
+        private val DEFAULT_ZOOM = 15f
     }
+
+    private var zoomedToLocation: Boolean = false
+
+    private val locationRequest: LocationRequest by lazy {
+        LocationRequest.create().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         runBlocking {
@@ -56,8 +72,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(binding.root)
         replaceFragment(Go())
 
-        val mapFrag = supportFragmentManager
-            .findFragmentById(R.id.mapFragment) as SupportMapFragment
+        val mapFrag = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFrag.getMapAsync(this)
 
         binding.BottomNavigationView.setOnItemSelectedListener { item ->
@@ -65,38 +80,95 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 R.id.Gonav -> replaceFragment(Go())
                 R.id.Community -> replaceFragment(Community())
                 R.id.Account -> replaceFragment(Account())
-                else -> {
-                }
             }
             true
         }
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
-        setUpMap()
+        checkForLocationPermission()
+        mMap.isMyLocationEnabled = true
+        setUpLocationCallback()
+        startLocationUpdates()
     }
 
-    private fun setUpMap(){
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED)
-            { ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE)
-            return
+    private fun checkForLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_REQUEST_CODE
+            )
         }
+    }
 
-        mMap.isMyLocationEnabled = true
-        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
-            if (location != null)
-                lastLocation = location
-            val currentLatLong = LatLng(location.latitude, location.longitude)
-            mMap.setMyLocationEnabled(true)
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 14f))
+    private fun setUpLocationCallback() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                for (location in locationResult.locations) {
+                    if (!zoomedToLocation && locationResult.locations.isNotEmpty())  {
+                        val currentLatLong = LatLng(location.latitude, location.longitude)
+                        zoomedToLocation = true
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, DEFAULT_ZOOM))
+                    }
+                }
+            }
         }
+    }
+
+    private fun startLocationUpdates() {
+        val locationSettingsRequest = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+            .build()
+
+        val settingsClient = LocationServices.getSettingsClient(this)
+        settingsClient.checkLocationSettings(locationSettingsRequest).addOnSuccessListener {
+            checkForLocationPermission()
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                null
+            )
+        }.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException) {
+                try {
+                    // Show dialog to enable location settings
+                    exception.startResolutionForResult(
+                        this@MainActivity,
+                        LOCATION_SETTINGS_REQUEST_CODE
+                    )
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == LOCATION_SETTINGS_REQUEST_CODE) {
+           startLocationUpdates()
+        }
+    }
+
+    private fun showLocationServicesRequiredDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Location Services Required")
+            .setMessage("Please enable location services to show your current location.")
+            .setPositiveButton("OK") { _, _ ->
+                val settingsIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivityForResult(settingsIntent, LOCATION_SETTINGS_REQUEST_CODE)
+            }
+            .setCancelable(false)
+            .show()
     }
 
     private fun replaceFragment(fragment: Fragment) {
@@ -105,7 +177,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         fragmentTransaction.replace(R.id.mapFragment, fragment)
         fragmentTransaction.commit()
     }
-
 
     fun showSharedRoutesDialog(view: View) {
         // Handle the onClick event for SharedRoutes button
