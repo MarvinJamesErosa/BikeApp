2a3feeb8931d91c831dc1df7401fc271432ef5aa
MarvinJamesErosa <mjae0109@gmail.com>
2023-07-07 11:03:43 +0800

    Add Navigation from Signup to Login

--- BikeBuddy/app/src/main/java/com/example/bikebuddy/SignUp.kt
+++ BikeBuddy/app/src/main/java/com/example/bikebuddy/SignUp.kt
@@ -1,13 +1,23 @@
 package com.example.bikebuddy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
+        val navigateToLoginButton: TextView = findViewById(R.id.signup_footer_btn)
 
+        navigateToLoginButton.setOnClickListener {
+            navigateToLogin()
+        }
+    }
+    private fun navigateToLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
